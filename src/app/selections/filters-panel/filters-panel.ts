e634561338b81cbb45873fc3ca467bf196d96d6b
Davide <rondey90@gmail.com>
2025-09-01 13:03:13 +0200

    Added tailwind. Also, added the UI of the filters panel.

--- src/app/selections/filters-panel/filters-panel.ts
+++ src/app/selections/filters-panel/filters-panel.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-import { MatGridList, MatGridTile } from '@angular/material/grid-list';
+import { Component, inject } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { FieldFilter } from '../models/field-filter';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { Validators } from '@angular/forms';
 
+const defaultFilterForm = {
+  selectedField: FieldFilter.nessuno,
+  value: '',
+};
 @Component({
   selector: 'app-filters-panel',
-  imports: [MatGridList, MatGridTile],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './filters-panel.html',
   styleUrl: './filters-panel.css',
 })
-export class FiltersPanel {}
+export class FiltersPanel {
+  private formBuilder = inject(FormBuilder);
+
+  filterForm = this.formBuilder.group({
+    selectedField: [defaultFilterForm.selectedField, Validators.required],
+    value: [defaultFilterForm.value],
+  });
+
+  onSubmit() {
+    if (this.filterForm.invalid) return;
+    console.log(this.filterForm.value);
+  }
+
+  onReset() {
+    this.filterForm.reset();
+
+    // Required, otherwise the form appears invalid
+    this.filterForm.clearValidators();
+    console.log(this.filterForm);
+  }
+}
---
+++ src/app/selections/models/field-filter.ts
@@ -0,0 +1,6 @@
+export enum FieldFilter {
+  nessuno = '',
+  codiceMinisteriale = 'codiceMinisteriale',
+  codiceInterno = 'codiceInterno',
+  descrizioneEsame = 'descrizioneEsame',
+}
